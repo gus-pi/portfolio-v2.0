cadab3038d680b65996524a364cd967c9b6afbba
PieMenard <pinedo.m.gustavo@gmail.com>
2025-10-08 01:25:41 -0400

    add global css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import './app.scss';
+
 const App = () => {
-    return <div>Hello World</div>;
+    return (
+        <div>
+            <section>hero</section>
+            <section>parallax</section>
+            <section>services</section>
+            <section>portfolio1</section>
+            <section>portfolio2</section>
+            <section>portfolio3</section>
+            <section>contact</section>
+        </div>
+    );
 };
 
 export default App;
