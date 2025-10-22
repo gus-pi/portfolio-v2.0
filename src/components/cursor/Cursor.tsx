d73ed3202a14621de1c671b586e79f419fe54f41
PieMenard <pinedo.m.gustavo@gmail.com>
2025-10-22 01:08:56 -0400

    add animated mouse cursor

---
+++ src/components/cursor/Cursor.tsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+import './cursor.scss';
+import { motion } from 'motion/react';
+
+const Cursor = () => {
+    const [position, setPosition] = useState({ x: 0, y: 0 });
+
+    useEffect(() => {
+        const mouseMove = (e: MouseEvent) => {
+            setPosition({ x: e.clientX, y: e.clientY });
+        };
+
+        window.addEventListener('mousemove', mouseMove);
+
+        return () => {
+            window.removeEventListener('mousemove', mouseMove);
+        };
+    }, []);
+
+    return (
+        <motion.div
+            className="cursor"
+            animate={{ x: position.x - 20, y: position.y - 20 }}
+        ></motion.div>
+    );
+};
+export default Cursor;
